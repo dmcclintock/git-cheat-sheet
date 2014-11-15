95b31402aa1376d048d267fa6fb7e3b9d3019ac8
Daniel McClintock <dmcclintock@acreativecollective.co>
2014-11-14 21:23:43 -0500

    Added Bower & Grunt packages

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		project: {
+			// assets: ['assets'],
+			css: ['sass/style.scss']
+		},
+
+		sass: {
+			dev: {
+				options: {
+					style: 'nested' //nested, compact, compressed, expanded
+				},
+				files: {
+					'css/style.css': 'sass/style.scss'
+				}
+			}
+		},
+
+		concat: {
+			fastclick: {
+				src: ['node_modules/highlight.js/styles/tomorrow-night-eighties.css'],
+				dest: 'sass/_highlight.scss'
+			}
+		},
+
+		// uglify: {
+		// 	dist: {
+		// 		files: {
+		// 			'js/highlight.min.js': ['node_modules/highlight.js/lib/index.js']
+		// 			// ,'js/custom.min.js': ['js/custom/*.js']
+		// 		}
+		// 	}
+		// },
+
+		watch: {
+			sass: {
+				files: 'sass/{,*/}*.{scss,sass}',
+				tasks: ['sass:dev']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [
+		'watch'
+	]);
+}
+
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>GIT Cheat Sheet</title>
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/styles/default.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/highlight.min.js"></script>
+	<script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+	<h1>Common git workflow commands</h1>
+	<h2>1. Fork the orignal repo</h2>
+	<h2>2. Clone the forked repo</h2>
+
+<pre class="acc">
+<in><cmd>cd</cmd> /Users/danielsmcclintock/acc/viewrc/acc-git</in>
+<in><cmd>git clone</cmd> https://github.com/danielsmcclintock/<em>FORKED_REPO</em></in>
+<out>Cloning into `FORKED_REPO`...
+remote: Counting objects: 10, done.
+remote: Compressing objects: 100% (8/8), done.
+remove: Total 10 (delta 1), reused 10 (delta 1)
+Unpacking objects: 100% (10/10), done.</out>
+</pre>
+
+</body>
+</html>
